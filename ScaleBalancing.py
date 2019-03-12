cea0cb3bd43dd09e082cf99204302133fd8f698d
Henry Xue <henryxue@gmail.com>
2019-03-12 17:10:19 -0400

    Update ScaleBalancing.py
    Rework exception handling

--- ScaleBalancing.py
+++ ScaleBalancing.py
@@ -1,5 +1,6 @@
 import ast
 import itertools
+import sys
 
 debug_Mode = False
 
@@ -30,40 +31,36 @@ def checkInput(list_Input):
     debug_Print(scale_Weight)
 
     if (len(scale_Weight) > 2):
-        print("Error: Please only enter 2 weights for the Balance Scale (left and right side weights)")
-        return
+        return "Error: Please only enter 2 positive weights for the Balance Scale (left and right side weights)"
     elif (len(scale_Weight) < 2):
-        print("Error: Please enter 2 weights for the Balance Scale (left and right side weights)")
-        return
+        return "Error: Please enter 2 positive weights for the Balance Scale (left and right side weights)"
 
     for i in scale_Weight:
+        if (i < 0):
+            return "Error: Please enter positive numeric values for the Balance Scale (left and right side weights)"
         try:
             int(i)
         except ValueError:
-            print("Error: Please enter numeric values for the Balance Scale (left and right side weights)")
-            return
+            return "Error: Please enter positive numeric values for the Balance Scale (left and right side weights)"
         
     ## Check Given Weight Inputs
     given_Weight = sorted(ast.literal_eval(list_Input[1]))
     debug_Print(given_Weight)
 
     if (len(given_Weight) < 1):
-        print("Error: Please enter at least 1 numeric weight for the Weight Balance")
-        return
+        return "Error: Please enter at least 1 positive numeric weight for the Weight Balance"
 
     for i in given_Weight:
+        if (i == 0):
+            return "Error: Please enter positive numeric value for the Weight Balance (excluding 0)"
+        if (i < 0):
+            return "Error: Please enter positive numeric value for the Weight Balance (excluding 0)"
         try:
             int(i)
         except ValueError:
-            print("Error: Please enter numeric values for the Weight Balance (excluding 0)")
-            return
-        
-    for i in given_Weight:
-        if (i == 0):
-            print("Error: Please enter numeric value for the Weight Balance (excluding 0)")
-            return
+            return "Error: Please enter positive numeric values for the Weight Balance (excluding 0)"
 
-    return scale_Weight, given_Weight
+    return determineBalance(scale_Weight, given_Weight)
 
 def main():
     ##"[3, 4]", "[7, 7, 2, 1]"
@@ -71,9 +68,7 @@ def main():
     ##"[13, 4]", "[14, 6, 3, 2, 1]"
     ##"[8, 3]", "[8]"
     list_Input = input("Input:")
-    scale_Weight, given_Weight = checkInput(ast.literal_eval(list_Input))
-
-    determine_Balance = determineBalance(scale_Weight, given_Weight)
+    determine_Balance = checkInput(ast.literal_eval(list_Input))
     print("Output:\"" + str(determine_Balance) + "\"")
 
 if __name__ == "__main__":
