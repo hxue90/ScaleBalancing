7d33654c7f2d41d04244ef7275c6009a754ef13d
Henry Xue <henryxue@gmail.com>
2019-03-12 12:38:59 -0400

    Create ScaleBalancing.py
    Request Input

---
+++ ScaleBalancing.py
@@ -0,0 +1,10 @@
+def requestInput():
+    list_Input = input("Input: ")
+    list_Input = tuple(list_Input)
+    print(type(list_Input))
+
+def main():
+    ## Requesting Input
+    requestInput()
+
+main()
