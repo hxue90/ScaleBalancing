ab7a5757eefe80607e7d5214692291406bbd8247
Henry Xue <henryxue@gmail.com>
2019-03-12 14:38:10 -0400

    Update ScaleBalancing.py
    Change from RequestInput to CheckInput

--- ScaleBalancing.py
+++ ScaleBalancing.py
@@ -6,20 +6,38 @@ def debug_Print(msg):
     if debug_Mode == True:
         print(msg)
 
-def requestInput():
-    list_Input = input("Input: ") ##"[3, 5, 4]", "[1, 2, 7, 7]"
-    list_Input = ast.literal_eval(list_Input)
-    
+def checkInput(list_Input):
     ## Check Scale Weight Inputs
     scale_Weight = ast.literal_eval(list_Input[0])
     debug_Print(scale_Weight)
-    
+
+    if (len(scale_Weight) > 2):
+        print("Please only enter 2 weights for the Balance Scale (left and right side weights)")
+        return
+    elif (len(scale_Weight) < 2):
+        print("Please enter 2 weights for the Balance Scale (left and right side weights)")
+        return
+
+    for i in scale_Weight:
+        try:
+            int(i)
+        except ValueError:
+            print("Please enter numeric values for the Balance Scale (left and right side weights)")
+            return
     ## Check Given Weight Inputs
     given_Weight = ast.literal_eval(list_Input[1])
     debug_Print(given_Weight)
 
-def main():
-    ## Requesting Input
-    requestInput()
+    for i in given_Weight:
+        try:
+            int(i)
+        except ValueError:
+            print("Please enter numeric values for the Weight Balance")
+            return
 
-main()
+def main():
+    list_Input = input("Input: ") ##"[3, 4]", "[1, 2, 7, 7]"
+    checkInput(ast.literal_eval(list_Input))
+
+if __name__ == "__main__":
+    main()
