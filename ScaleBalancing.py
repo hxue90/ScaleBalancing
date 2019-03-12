b8c88cf0fd1c32e0d62a25e69333f700acb47d50
Henry Xue <henryxue@gmail.com>
2019-03-12 14:05:00 -0400

    Update ScaleBalancing.py
    1. Converting Input to Tuple

--- ScaleBalancing.py
+++ ScaleBalancing.py
@@ -1,7 +1,22 @@
+import ast
+
+debug_Mode = True
+
+def debug_Print(msg):
+    if debug_Mode == True:
+        print(msg)
+
 def requestInput():
-    list_Input = input("Input: ")
-    list_Input = tuple(list_Input)
-    print(type(list_Input))
+    list_Input = input("Input: ") ##"[3, 5, 4]", "[1, 2, 7, 7]"
+    list_Input = ast.literal_eval(list_Input)
+    
+    ## Check Scale Weight Inputs
+    scale_Weight = ast.literal_eval(list_Input[0])
+    debug_Print(scale_Weight)
+    
+    ## Check Given Weight Inputs
+    given_Weight = ast.literal_eval(list_Input[1])
+    debug_Print(given_Weight)
 
 def main():
     ## Requesting Input
